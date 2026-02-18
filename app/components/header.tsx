8d0cd1f7b2f87c2a13f566aed923a02ce2d94b31
jes4l <jesalvadgama@gmail.com>
2026-02-18 11:54:06 +0000

    code: initial commit

---
+++ app/components/header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <h1>Unstable Cat Games</h1>
+    </header>
+  );
+};
+
+export default Header;
---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Header from "./components/header";
+
+export default function Page() {
+  return (
+    <main>
+      <Header />
+      <p>Welcome to our site!</p>
+    </main>
+  );
+}
