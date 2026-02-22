492897add89cdc20de32a57cd9a595b8143d8729
jes4l <jesalvadgama@gmail.com>
2026-02-22 22:44:05 +0000

    code: changed the about me curve, asset and colour

--- app/page.tsx
+++ app/page.tsx
@@ -93,11 +93,6 @@ export default async function Page() {
         <AboutMe />
 
         <section className="relative bg-[#0e172a]">
-          <div className="absolute top-0 left-0 w-full overflow-hidden leading-[0] -translate-y-[99%] z-10">
-            <svg viewBox="0 0 1200 120" preserveAspectRatio="none" className="relative block w-full h-[100px] fill-[#0e172a]">
-              <path d="M0,0V46.29c47.79,22.2,103.59,32.17,158,28,70.36-5.37,136.33-33.31,206.8-37.5C438.64,32.43,512.34,53.67,583,72.05c69.27,18,138.3,24.88,209.4,13.08,36.15-6,69.85-17.84,104.45-29.34C989.49,25,1113-14.29,1200,52.47V120H0Z" />
-            </svg>
-          </div>
           <Bundle bundleImages={bundleImages} />
         </section>
 
